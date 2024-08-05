69b5404a198126398daf0911d5b000fbd8a72bcb
ryu-hp <ryupon34689@gmail.com>
2024-08-06 04:10:54 +0900

    add works

--- js/script.js
+++ js/script.js
@@ -5,4 +5,20 @@ jQuery(function ($) {
     $(findElm).toggleClass('open');//アコーディオンの上下動作
     $(this).toggleClass('active');
   });
+
+  let mySwiper = new Swiper ('#works-swiper', {
+    // オプション設定
+    slidesPerView: 'auto',
+    loop: true,
+    centeredSlides: true, // アクティブなスライドを中央に配置
+    //オートプレイ
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    }, 
+    navigation: {
+      nextEl: '.swiper-button-next', // 「次へ」ボタン要素のクラス
+      prevEl: '.swiper-button-prev', // 「前へ」ボタン要素のクラス
+    },  
+  });
 });
