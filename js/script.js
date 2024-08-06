de951292703f89504a42af1b04d8b9af6721ffe5
ryu-hp <ryupon34689@gmail.com>
2024-08-07 08:04:42 +0900

    add flow

--- js/script.js
+++ js/script.js
@@ -21,7 +21,24 @@ jQuery(function ($) {
       prevEl: '.swiper-button-prev', // 「前へ」ボタン要素のクラス
     },  
   });
-  
+  let flowSwiper = new Swiper ('#flow-swiper', {
+    // オプション設定
+    slidesPerView: 'auto',
+    loop: true,
+    centeredSlides: true, // アクティブなスライドを中央に配置
+    //オートプレイ
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },  
+    //ページネーション表示設定
+    pagination: {
+      el: '#flow-swiper_pagination', //ページネーション要素
+      type: 'bullets', //ページネーションの種類
+      clickable: true, //クリックに反応させる
+    },
+  });
+
   let accordionDetails = '.js-details';
   let accordionSummary = '.js-details-summary';
   let accordionContent = '.js-details-content';
